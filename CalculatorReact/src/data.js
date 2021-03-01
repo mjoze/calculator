ca00999e2152017da1855b9b75200b075cfb6c40
mjoze <mirek.jozefiak@gmail.com>
2021-03-01 20:05:11 +0100

    start project react

---
+++ CalculatorReact/src/data.js
@@ -0,0 +1,33 @@
+const buttons = [
+    { name: '%', value: 'percent' },
+    { name: 'CE', value: 'clear' },
+    { name: 'C', value: 'clear' },
+    { name: 'DEL', value: 'delete' },
+
+    { name: '1/x', value: '' },
+    { name: 'x2', value: '' },
+    { name: '/2', value: '' },
+    { name: '/', value: 'divide' },
+
+    { name: '7', value: 'number' },
+    { name: '8', value: 'number' },
+    { name: '9', value: 'number' },
+    { name: 'X', value: 'multiply' },
+
+    { name: '4', value: 'number' },
+    { name: '5', value: 'number' },
+    { name: '6', value: 'number' },
+    { name: '-', value: 'subtraction' },
+
+    { name: '1', value: 'number' },
+    { name: '2', value: 'number' },
+    { name: '3', value: 'number' },
+    { name: '+', value: 'add' },
+
+    { name: '+/-', value: 'sign' },
+    { name: '0', value: 'number' },
+    { name: ',', value: 'coma' },
+    { name: '=', value: 'equal' },
+]
+
+export default buttons;
